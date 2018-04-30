afa753f2155a143c13771958d857aa1262e830c6
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2018-05-01 00:56:18 +0200

    Fix touch.

--- fireworlds-game/arm7/source/main.cpp
+++ fireworlds-game/arm7/source/main.cpp
@@ -44,9 +44,10 @@ int main() {
 //---------------------------------------------------------------------------------
 //	systemMsgHandler(0, 0);
 	readUserSettings();
-	
+
 	irqInit();
 	fifoInit();
+    touchInit();
 	enableSound();
 	AS_Init();
 
@@ -60,20 +61,20 @@ int main() {
 	installSoundFIFO();
 
 	installSystemFIFO();
-	
+
 	irqSet(IRQ_VCOUNT, VcountHandler);
 	irqSet(IRQ_VBLANK, VblankHandler);
 
-	irqEnable( IRQ_VBLANK | IRQ_VCOUNT | IRQ_NETWORK);   
+	irqEnable( IRQ_VBLANK | IRQ_VCOUNT | IRQ_NETWORK);
 
 	// Keep the ARM7 mostly idle
 	while (!exitflag) {
-		
+
 		if ( 0 == (REG_KEYINPUT & (KEY_SELECT | KEY_START | KEY_L | KEY_R))) {
 			exitflag = true;
 		}
 		timerStart(2, ClockDivider_1024, 0, 0);
-		AS_MP3Engine(); 
+		AS_MP3Engine();
 		int shit = BUS_CLOCK / 100 / 60 / 1024;
 		int dd = timerElapsed(2)/shit;
 		PA_Transfer->cpuUsage = dd;
